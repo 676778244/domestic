2115b9a08d2313575892c7dde4366651462c594d
yixiaodong <yixiaodong@guoshanghz.com>
2021-09-07 11:32:00 +0800

    支持跨域

---
+++ src/main/java/com/huilong/domestic/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.huilong.domestic.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig implements WebMvcConfigurer {
+    /**
+     * 跨域支持
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowedHeaders("*")
+                .maxAge(3600 * 24);
+    }
+}
