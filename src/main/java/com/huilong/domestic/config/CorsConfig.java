32395ac281d1be01b516fba86dd14da2b504db2a
yixiaodong <yixiaodong@guoshanghz.com>
2021-09-07 12:38:26 +0800

    支持跨域

--- src/main/java/com/huilong/domestic/config/CorsConfig.java
+++ src/main/java/com/huilong/domestic/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .allowedHeaders("*")
